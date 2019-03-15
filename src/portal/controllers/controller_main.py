6962c131ff44bd3ad970c0a507bbc1e5aae3a28f
kwan <20851768+kwannn12@users.noreply.github.com>
2019-03-15 11:55:50 +0100

    fix map routing

--- src/portal/controllers/controller_main.py
+++ src/portal/controllers/controller_main.py
@@ -6,16 +6,18 @@ from portal.view import View
 
 main_controller = Blueprint('main', __name__)
 
+
 @main_controller.route('/')
 def index():
-    return View(main_controller, resources={
+    return View(main_controller, view="index", resources={
         "index.css": Resource.StyleSheet
     }).render()
+
 
 @main_controller.route('/map')
 @auth_required
 def map():
-    return View(main_controller, resources={
+    return View(main_controller, view="map", resources={
         "map.js": Resource.Script,
         "map.css": Resource.StyleSheet
     }).render()
