8c970b1a11cf4e873686e403854448fd58a8319e
kwan <20851768+kwannn12@users.noreply.github.com>
2019-03-24 15:36:54 +0100

    add alternative code for current geolocation

--- src/portal/frontend/views/main/resources/map.js
+++ src/portal/frontend/views/main/resources/map.js
@@ -19,6 +19,8 @@ map_element.invalidateSize();
 //     }).addTo(map);
 
 // Locate
+// variable to contain current location
+var currentLocation = [0, 0]
 // on locate, zoom and keep watch
 map_element.locate({setView: true,
 			maxZoom: 16,
@@ -27,10 +29,27 @@ map_element.locate({setView: true,
 
 map_element.on('locationfound', function(e) {
 	var radius = e.accuracy / 10;
+	console.log(e.latlng)
 	L.marker(e.latlng).addTo(map_element)
 		.bindPopup("You are within " + radius + " meters from this point").openPopup();
 	L.circle(e.latlng, radius).addTo(map_element);
 });
+
+
+// // GEOLOCATION ALTERNATIVE
+//navigator.geolocation.getCurrentPosition(function(location) {
+//  var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+//
+//  var mymap = L.map('mapid').setView(latlng, 13)
+//  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
+//    maxZoom: 18,
+//    id: 'mapbox.streets',
+//    accessToken: 'pk.eyJ1IjoiYmJyb29rMTU0IiwiYSI6ImNpcXN3dnJrdDAwMGNmd250bjhvZXpnbWsifQ.Nf9Zkfchos577IanoKMoYQ'
+//  }).addTo(mymap);
+//
+//  var marker = L.marker(latlng).addTo(mymap);
+//});
 
 
 // INTERACTION WITH MAP
